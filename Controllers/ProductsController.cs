619a2dcf8d89bccd9375295495f8946662878400
ccarlson41 <ccarlson41@huskers.unl.edu>
2024-04-12 11:47:10 -0500

    Attempt to load list of products to home in `homecontrolller.cs`

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Online_store.Models;
 using Online_store.Data;
 using System.Linq;
 
@@ -12,10 +14,29 @@ namespace Online_store.Controllers
         {
             _context = context;
         }
+
         public IActionResult Index()
         {
             var products = _context.Products.ToList();
             return View(products);
+        }
+
+        // Action to retrieve all products from the database
+        public IActionResult Retrieve()
+        {
+            List<ProductModel> products = _context.Products.ToList();
+            return Json(products);
+        }
+
+        // Action to retrieve a specific product by id from the database
+        public IActionResult Details(int id)
+        {
+            ProductModel product = _context.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Json(product);
         }
     }
 }
