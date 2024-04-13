295b673efe5181e2db201483588dd7b20a131dcf
[Amgad Ahmed] <[aahmed12@huskers.unl.edu]>
2024-04-13 16:51:48 -0500

    added methods to Login Controller

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -27,6 +27,55 @@ namespace Online_store.Controllers
             return View(users);
         }
 
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new UserModel()
+            {
+                ReturnUrl = returnUrl
+            }) ;
+        }
 
+        [HttpPost]
+        public async Task<IActionResult> Login(UserModel userModel)
+        {
+            if (!ModelState.IsValid)
+                return View(userModel);
+            var user = await _userManager.FindByNameAsync(userModel.Username);
+            if(user != null)
+            {
+                var result = await _signInManager.PasswordSignInAsync(user, userModel.PasswordHash, false, false);
+                if (result.Succeeded)
+                {
+                    if (string.IsNullOrEmpty(userModel.ReturnUrl))
+                        return RedirectToAction("Index", "Home");
+                    return Redirect(userModel.ReturnUrl);
+                }
+            }
+            ModelState.AddModelError("", "Username/password not found");
+            return View(userModel); 
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(UserModel userModel)
+        {
+            if(ModelState.IsValid) {
+                var user1 = new IdentityUser() { UserName = userModel.Username };
+                var result = await _userManager.CreateAsync(user1, userModel.PasswordHash); 
+                if(result.Succeeded) {
+                    return RedirectToAction("Index", "Home");
+                } 
+            }
+            return View(userModel);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
     }
   }
--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -10,12 +10,16 @@ public partial class UserModel
     [Key]
     public int UserId { get; set; }
 
+    [Required]
+    [Display(Name = "User Name")]
     public string Username { get; set; }
 
     public string Email { get; set; }
 
+    [Required]
+    [DataType(DataType.Password)]   
     public string PasswordHash { get; set; }
-
+    public string ReturnUrl {  get; set; }
     public int ShippingAddressId { get; set; }
 
     public int BillingAddressId { get; set; }
