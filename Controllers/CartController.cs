017ad475a820d3e1711ae912ee914364da24277a
David Khuu <dkhuu2@huskers.unl.edu>
2024-04-26 01:15:11 -0500

    fixed product page bugs relating to header/footer. moved cart to its own section under views. add products to nav bar

---
+++ Controllers/CartController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Online_store.Models;
+using Online_store.Data;
+using System.Linq;
+
+namespace Online_store.Controllers
+{
+    public class CartController
+{
+    //TODO: Implement a cart controller to get the cart page working.
+}
+}
