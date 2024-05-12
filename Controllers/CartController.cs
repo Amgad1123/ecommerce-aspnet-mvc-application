1a363f83b15394053486eca236da2034a98a1a51
ccarlson41 <ccarlson41@huskers.unl.edu>
2024-05-11 21:40:16 -0500

    Added a confirmation and Thank You page for when you checkout.

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -74,6 +74,25 @@ namespace Online_store.Controllers
             return RedirectToAction("Index");
         }
 
+        [HttpPost]
+        public IActionResult ClearCart()
+        {
+            var ProductsInCart = _context.Products.Where(p => p.NumInCart > 0).ToList();
+
+            foreach (var product in ProductsInCart)
+            {
+                product.NumInCart = 0;
+                _context.SaveChanges();
+
+            }
+
+            _boughtItems.Clear(); // Clear the list of bought items
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
 
         public static decimal CalculateSubtotal(List<ProductModel> items)
         {
